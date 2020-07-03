f4111c0b67c3f1e19fba15511ceef89ed50aa77d
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-07-03 21:41:44 +0800

    feature
    添加ignore文件，忽略sonar扫描报告

--- sonar/dotnetsonar.cmd
+++ sonar/dotnetsonar.cmd
@@ -1,4 +1,5 @@
-cs csproj
-dotnet-sonarscanner  begin /key:"GrpcService" /name:"GrpcService" /version:"1.0"
+cd csproj
+dotnet sonarscanner  begin /key:"GrpcService" /name:"GrpcService" /version:"1.0"
 dotnet build
 dotnet sonarscanner end
+pause
