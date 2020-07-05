fd8bd4a83cdd440f2ef88770639e0de713e0bc19
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-07-05 16:48:41 +0800

    doc
    删除MSBuild完整路径，使用相对路径

--- sonar/cs.cmd
+++ sonar/cs.cmd
@@ -2,7 +2,7 @@ cd csproj
 
 SonarScanner.MSBuild begin /key:"GrpcService" /name:"GrpcService" /version:"1.0"
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" /t:rebuild
+MSBuild.exe /t:rebuild
 
 SonarScanner.MSBuild end
 
