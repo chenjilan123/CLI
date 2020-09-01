f8e2dd0fcf6fccea81813d1fa78b6d8fc30d53b6
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-09-01 21:29:21 +0800

    Update file.sh

--- Base/file.sh
+++ Base/file.sh
@@ -9,3 +9,13 @@ touch f4.txt
 #批量移动文件
 find -name '*.txt' -exec mv {} ./dir2 \; 
 #find -name '*.txt' -exec mv {} J:/CLI/base/dir1/dir2/ \; 
+
+#删除文件夹
+rm -r 1
+#删除文件
+rm -f 1.txt
+#or
+rm 1.txt
+
+#权限
+chmod -R 775 dir
