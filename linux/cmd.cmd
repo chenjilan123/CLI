f0231f376305df2e03e3af8be2e94ca4eb4af905
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-10-10 13:38:36 +0800

    feature
    关闭进程命令

--- linux/cmd.cmd
+++ linux/cmd.cmd
@@ -1,9 +1,28 @@
-后台开启服务：
+#!后台开启服务：
 nohup dotnet AlarmCenterGrpcServiceLibrary.dll > /dev/null 2>&1 &
 nohup dotnet IoTCenterWebApi.dll > /dev/null 2>&1 &
 
-查看进程：
+#!查看进程：
 ps -ef|grep dotnet
 
-关闭进程：
+#!关闭进程：
 kill -9  进程号
+
+#!ss命令
+ss -lntup
+
+#!nc命令
+nc 127.0.0.1 22
+
+#!ssh服务的启动与安装
+
+#!安装
+sudo apt-get install ssh
+#!启动
+service sshd start
+#!配置
+vim /etc/ssh/sshd_config
+
+#!vim修改只读文件
+w !sudo tee %
+q!
